bd5f0d6f8636849f073b424fa7e7775831814776
shouq <shouqbis@gmail.com>
2025-05-11 13:15:39 +0300

    finish

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
 let container = document.getElementById("container")
-
-fetch("https://coffee.alexflipnote.dev/random.json")
-  .then((res) => res.json())
-  .then((data) => {
     let img = document.createElement("img")
-    img.width = 200
+ img.width = 200
+
 
     let change = () => {
       fetch("https://coffee.alexflipnote.dev/random.json")
         .then((res) => res.json())
-        .then((imgData) => {
-          img.src = imgData.file
+        .then((Data) => {
+          img.src = Data.file
         })
     }
 
     container.appendChild(img)
 
     setInterval(change, 3000)
-  })
+
